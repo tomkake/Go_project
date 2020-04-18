049005f86c575ad212a93c8bde4f4d3e8c65a693
Kakeru TAMURA <s1f101703241@iniad.org>
2020-04-18 15:58:51 +0900

    end display html go+gin

--- go_gin_tutrial/hello.go
+++ go_gin_tutrial/hello.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	fmt.Println(http.StatusOK)
+	r := gin.Default()
+	r.LoadHTMLGlob("template/*.html")
+	r.GET("/hello", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "hello.html", gin.H{})
+	})
+	r.Run()
 	fmt.Println("Hello")
 }
